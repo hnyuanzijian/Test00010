5841324ae0c99e73f0690998642c1dbc18e572d3
hnyuanzijian <18738395879@163.com>
2017-05-27 17:11:58 +0800

    多态，异常

---
+++ src/com/yuanzijian07/Demo7.java
@@ -0,0 +1,40 @@
+package com.yuanzijian07;
+
+//实现关系下匿名内部类
+interface Dao{
+
+	public void add();
+}
+
+
+class Outer{
+
+	public void print(){
+		//创建一个匿名内部类的对象
+		new Dao(){   //不是接口不能创建对象吗？怎么现在又可以了？
+			 
+			 public void add(){
+				System.out.println("添加成功");
+			 }
+		}.add();
+	}
+}
+
+class Demo7 
+{
+	public static void main(String[] args) 
+	{
+		test(new Dao(){
+			
+			public void add(){
+				System.out.println("添加员工成功");
+			}
+		});
+	}
+
+	//调用这个方法...
+	public static void  test(Dao d){  //形参类型是一个接口引用..
+		d.add();
+	}
+}
+
